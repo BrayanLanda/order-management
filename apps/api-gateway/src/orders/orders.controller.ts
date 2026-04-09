d42b099aada132de58a8eb28ef7c51bfe78ad8fc
Brayan Landa <landakkpp@gmail.com>
2026-04-09 02:09:11 -0500

    feat: implement order creation endpoint via microservice message pattern

--- apps/api-gateway/src/orders/orders.controller.ts
+++ apps/api-gateway/src/orders/orders.controller.ts
@@ -1,6 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { MICROSERVICES_CLIENTS } from 'src/constans';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('orders')
 export class OrdersController {
-    
+    constructor(
+        @Inject(MICROSERVICES_CLIENTS.ORDERS_SERVICE)
+        private readonly ordersClient: ClientProxy,
+    ) {}
+
+    @Post()
+    createOrder(@Body() order: any) {
+        return this.ordersClient.send('create_order', order);
+    }
 }
--- apps/orders-service/src/orders/orders.controller.ts
+++ apps/orders-service/src/orders/orders.controller.ts
@@ -1,7 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
+
+  @MessagePattern('create_order')
+  createOrder(order: any) {
+    return { message: 'Order created', order };
+  }
 }
